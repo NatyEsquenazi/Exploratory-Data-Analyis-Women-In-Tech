68da8f970f3ee0e00a6bd9ffdda65e3177dfced0
Naty Esquenazi <naty.esquenazi@gmail.com>
2020-11-17 10:19:11 -0300

    Comienzo de analisis

--- Prueba.R
+++ Prueba.R
@@ -2,46 +2,55 @@
 library(tidyverse)
 library(dplyr)
 library(readxl)
+library(readr)
 library(janitor)
 library(funModeling)
+library(tidyr)
+library(summarytools)
 
 survey<- read_excel("results-survey724261 (1).xlsx")
+espe <- read_excel("espe.xlsx")
+
+df <- survey %>% 
+  select(-c(B.1, B.2))
+
+df <- left_join(df, espe, by='A.1')
 
 ############### Exploracion ############### ----------------------------
 
 # 1. Edad: 
 
 #En la variable edad 6.04% de las observaciones son nulas.
-df_status(survey$A.6)
+df_status(df$A.6)
 
-plot_1 <- ggplot(data = survey, mapping = aes(x=A.6)) +
+plot_1 <- ggplot(data = df, mapping = aes(x=A.6)) +
   geom_histogram(bins = 30) +
   labs(x = "Edad",
        title="Distribucion de edad",
        caption="Fuente: MET CBA")
 
 #Elimino nulos 
-survey<-subset(survey, !is.na(A.6))
+df<-subset(df, !is.na(A.6))
 
 #Me quedo con mayores a 18 anios
-survey <- survey %>% 
+df <- df %>% 
   filter(A.6 >= 18)
 
 # 2. Genero: 
 
-df_status(survey$A.8)
+df_status(df$A.8)
 
 #Elimino nulos 
-survey<-subset(survey, !is.na(A.7))
-survey<-subset(survey, !is.na(A.8))
+df<-subset(df, !is.na(A.7))
+df<-subset(df, !is.na(A.8))
 
-survey <- survey %>% 
+df <- df %>% 
   mutate(A.7=as.character(A.7),
          A.7 = case_when(A.7 == 'Other' ~ '1',
                    A.7 == 'Estoy en proceso de definir mi identidad' ~ '2',
                    A.7 == 'No tengo género' ~ '3'))
 
-survey <- survey %>% 
+df <- df %>% 
   mutate(A.8=as.character(A.8),
          A.8 = case_when(A.8 == 'mujer'~'1',
                    A.8 == 'Mujer'~'1',
@@ -70,46 +79,79 @@ survey <- survey %>%
 
 # 3. Residencia 
 
-unique(survey$A.9)
-survey<-subset(survey, !is.na(A.9))
+unique(df$A.9)
+df<-subset(df, !is.na(A.9))
 
 # 4. Especialidad: dejo los nulos xq pueden indicar no especialidad 
 
-unique(survey$B.1)
-survey <- survey %>% 
-  mutate(B.1=as.character(B.1),
-         B.1 = case_when(B.1 == 'Administración de sistemas'~'1',
-                         B.1 == 'Análisis de datos'~'2',
-                         B.1 == 'Desarrollo de software'~'3',
-                         B.1 == 'Implementación de software'~'3',
-                         B.1 == 'Diseño UX/UI'~'4',
-                         B.1 == 'Testing'~'5',
-                         B.1 == 'Seguridad informática'~'6',
-                         B.1 == 'UX Research'~'7',
-                         B.1 == 'Metodologías ágiles'~'8',
-                         B.1 == 'Análisis funcional'~'9',
-                         B.1 == 'Soporte técnico/al usuario'~'10',
-                         B.1 == 'Calidad de software'~'11',
-                         B.1 == 'Marketing Digital'~'12',
-                         B.1 == 'Accesibilidad'~'13',
-                         B.1 == 'Administración de bases de datos'~'14',
-                         B.1 == 'Desarrollo/Estrategia de producto digital'~'15',
-                         B.1 == 'Administración de redes y comunicaciones'~'16',
-                         B.1 == 'Other'~ '0'))
-unique(survey$B.2)
+#df <- df %>% 
+ # mutate(B.1=as.character(B.1),
+ #       B.1 = case_when(B.1 == 'Administración de sistemas'~'Sistemas',
+#                         B.1 == 'Análisis de datos'~'Datos',
+ #                        B.1 == 'Desarrollo de software'~'Software',
+  #                       B.1 == 'Implementación de software'~'Software',
+  #                       B.1 == 'Diseño UX/UI'~'Disenio',
+  #                       B.1 == 'Testing'~'Testing',
+  #                       B.1 == 'Seguridad informática'~'Seguridad',
+  #                       B.1 == 'UX Research'~'Investigacion',
+  #                       B.1 == 'Metodologías ágiles'~'Metodologias',
+  #                       B.1 == 'Análisis funcional'~'Funcional',
+  #                       B.1 == 'Soporte técnico/al usuario'~'Tecnico',
+  #                       B.1 == 'Calidad de software'~'Software',
+  #                       B.1 == 'Marketing Digital'~'Marketing',
+  #                       B.1 == 'Accesibilidad'~'Accesibilidad',
+  #                       B.1 == 'Administración de bases de datos'~'Datos',
+  #                       B.1 == 'Desarrollo/Estrategia de producto digital'~'Producto',
+  #                       B.1 == 'Administración de redes y comunicaciones'~'Comunicacion',
+  #                       B.1 == 'Other'~ 'Otro'))
 
-
-                         
-
-
-
-
-
-
-
-
-
-
+#survey <- survey %>% 
+#  mutate(B.2=as.character(B.2),
+#         B.2 = case_when(B.2 == 'Scrum master'~'Projecto',
+#                         B.2 == 'ACCESIBILIDAD'~'Accesibilidad',
+#                         B.2 == 'administracion'~'Adm',
+#                         B.2 == 'administrativo'~'Adm',
+#                         B.2 == 'ARQUITECTURA DE SOFTWARE'~'Software',
+#                         B.2 == 'Auditoría de Sistemas'~'Sistemas',
+#                         B.2 == 'Ciencia de datos'~'Datos',
+#                         B.2 == 'Comunicación'~'Comunicacion',
+#                         B.2 == 'Consultor BI/Soporte BI'~'Datos',
+#                         B.2 == 'contadora'~'Profesional',
+#                         B.2 == 'data entry'~'Datos',
+#                         B.2 == 'Data science'~'Datos',
+#                         B.2 == 'Data scientist'~'Datos',
+#                         B.2 == 'Derecho informático'~'Datos',
+#                         B.2 == 'Desarollador ETL'~'Datos',
+#                         B.2 == 'Desarrollo de base de datos'~'Datos',
+#                         B.2 == 'Desarrollo web'~'Web',
+#                         B.2 == 'Desarollador ETL'~'Datos',
+#                         B.2 == 'Devops, pero estudio programación y estoy estudiando front ahora'~'Devops',
+#                         B.2 == 'DevOps/SRE'~'Devops',
+#                         B.2 == 'Diseño de HW - Comunicaciones Digitales'~'Comunicacion',
+#                         B.2 == 'Diseño gráfico'~'Disenio',
+#                         B.2 == 'Educación + Tecnología'~'Projecto',
+#                         B.2 == 'Gestion de Capital Humano' ~'rrhh',
+#                         B.2 == 'it recruiter' ~ 'rrhh',
+#                         B.2 == 'lider de proyecto'~'Projecto',
+#                         B.2 == 'Líder de proyectos de software'~'Software',
+#                         B.2 == 'Manager'~'Projecto',
+#                         B.2 == 'Problem Solver'~'Projecto',
+#                         B.2 == 'Product Management'~'Projecto',
+#                         B.2 == 'Product Manager'~'Projecto',
+#                         B.2 == 'Profesional'~'Profesional',
+#                         B.2 == 'Programa manager'~'Projecto',
+#                         B.2 == 'project management'~'Projecto',
+#                         B.2 == 'Project Management'~'Projecto',
+#                         B.2 == 'Project Manager '~'Projecto',
+#                         B.2 == 'Redacción de contenidos digitales'~'Comunicacion',
+#                         B.2 == 'rr '~'rrhh',
+#                         B.2 == 'Seleccion'~'rrhh',
+#                         B.2 == 'Soporte al negocio'~'Projecto',
+#                         B.2 == 'Soporte Funcional de software'~'Software',
+#                         B.2 == 'ceo'~'Projecto',
+#                         B.2 == 'Testing, análisis funcional, soporte técnico/al usuario, Project  manager'~'Testing', 
+#                         B.2 == 'Visual Design & Dirección de Arte '~'Disenio',
+#                         B.2 == 'Análisis y desarrollo de sw'~ 'Software'))
 
 
 
--- analisis.R
+++ analisis.R
@@ -1,10 +1,40 @@
 
 ##################### Analisis #######################
 
-freq(survey$A.8)
+freq(df$A.8, cumul = FALSE)
+freq(df$B.1, cumul = FALSE)
+df$edad4t <- cut(df$A.6,
+                 breaks = c(min(df$A.6),
+                   25, 35, max(df$A.6)))
+freq(df$edad4t, report.nas = FALSE)
 
-# RTA: 93% de las encuestadas son mujeres cis, mientras que solo el 0.36% pertecen a categorias mujer/varon trans, bisexuales y queer. 
+# RTA: 93% de las encuestadas son mujeres cis, mientras que solo el 3.57% son hombres 
+# RTA: 39% de las encuestadas se dedican a la industria de software (implementacion, desarrollo, arquitectura), un 10.57% al disenio
+# RTA: un 75% de los encuestados tiene entre 25-35 anios o menos, un 16% tiene entre 18-25 y el resto (24%) pertenecen a 35 a 56 anios 
 
-genero_espec <- table(survey$A.8, survey$B.1)
 
-#RTA: el genero con la especialidad. De las mujeres encuestadas la mayoria tiene como especialidad desarrollo/implementacion de software. No hay una tendencia clara entre las especialidades menos frecuentes. 
+####  Existe una relacion entre el genero y la especialidad elegida? -----
+
+genero_espe <- table(df$A.8, df$B.1)
+round(prop.table(genero_espe), 3)* 100
+
+# RTA: existe una mayoria de mujeres con especialidad en software (implementacion, arquitectura) en un 36%. Luego disenio en un 10% y datos en un 9%. No hay una tendencia clara entre las especialidades menos frecuentes.  
+
+summary(df$A.6)
+
+#RTA: El promedio de edad registrado es de 31 anios 
+
+
+################## Representacion grafica de tablas #############
+
+mosaicplot(genero_espe, # Tabla de contingencias a graficar
+           main= "Especialidad y Sexo",
+           xlab="Sexo",
+           ylab="Especialidad",
+           col=c(2,3,4,5),
+           cex.axis=0.3,
+)
+
+
+
+
